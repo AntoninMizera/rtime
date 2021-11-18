13770f7aea470d1768431671c7f391c2ad230de0
AntoninMizera <91247749+AntoninMizera@users.noreply.github.com>
2021-11-18 10:03:49 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,62 @@
+const STEP = 250;
+let interval = 0, startTime = 0;
+
+function playToneForInterval() {
+    if (interval !== 0) {
+        basic.showIcon(IconNames.Target);
+        music.playTone(Note.CSharp, interval);
+        basic.clearScreen();
+    }
+}
+
+input.onButtonPressed(Button.B, () => {
+    interval = randint(1, 12) * STEP;
+    playToneForInterval();
+});
+
+input.onButtonPressed(Button.A, playToneForInterval);
+
+input.onLogoEvent(TouchButtonEvent.Touched, () => {
+    startTime = control.millis();
+    basic.showIcon(IconNames.House);
+});
+
+input.onLogoEvent(TouchButtonEvent.Released, () => {
+    const diffTime = control.millis() - startTime;
+    const intervalRatio = diffTime / interval;
+
+    console.logValue("diff", diffTime);
+    console.logValue("ratio", intervalRatio);
+
+    // Give the user a somewhat good tolerance of 90%-110%,
+    // just in case.
+    if (intervalRatio >= .9 && intervalRatio <= 1.1) {
+        basic.showIcon(IconNames.Yes)
+        music.playTone(Note.CSharp, 1000);
+        basic.clearScreen();
+    } else if (intervalRatio < .9) {
+        basic.showLeds(
+            `. . # . .
+             . . # . .
+             # . # . #
+             . # # # .
+             . . # . .`,
+            1000
+        );
+
+        led.plotBarGraph(diffTime, interval);
+    } else if (intervalRatio > 1.1) {
+        basic.showLeds(
+            `. . # . .
+             . # # # .
+             # . # . #
+             . . # . .
+             . . # . .`,
+            1000
+        );
+
+        // A string is shown instead, as I believe it would certainly be more 
+        // helpful than plotting a graph that tells the user nothing.
+        basic.showString(`+${Math.roundWithPrecision((diffTime - interval) / 1000, 2)}s`)
+    }
+})
